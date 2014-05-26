4cfbd08165950820a1f37e2a3724e4843c90733c
rubenwardy <rubenwardy@gmail.com>
2014-05-26 19:43:39 +0100

    Fix switch missing break

--- src/FileFormat/FileFormat.cpp
+++ src/FileFormat/FileFormat.cpp
@@ -7,8 +7,10 @@ FileFormat *getFromType(FileFormatType id, EditorState *st)
 	switch (id) {
 	case FILE_FORMAT_NBE:
 		return new NBEFileFormat(st);
+		break;
 	case FILE_FORMAT_LUA:
 		return new LuaFileFormat(st);
+		break;
 	}
 	return NULL;
 }
