f35db36995226645b68b56e108d4d5079a597285
rubenwardy <rubenwardy@gmail.com>
2014-05-26 19:44:37 +0100

    Remove unneeded breaks

--- src/FileFormat/FileFormat.cpp
+++ src/FileFormat/FileFormat.cpp
@@ -7,10 +7,8 @@ FileFormat *getFromType(FileFormatType id, EditorState *st)
 	switch (id) {
 	case FILE_FORMAT_NBE:
 		return new NBEFileFormat(st);
-		break;
 	case FILE_FORMAT_LUA:
 		return new LuaFileFormat(st);
-		break;
 	}
 	return NULL;
 }
